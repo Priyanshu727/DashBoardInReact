232ca02db7b1ff2fa25642fe34526cdc8a1129db
Priyanshu727 <priyanshumishra7272@gmail.com>
2024-04-10 18:50:34 +0530

    DashBoardInReact
    react final project

---
+++ src/firebase/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAICTHNnyZFSbI7Y5fiW07vL6EGw2-b0LY",
+  authDomain: "react-final-project-27904.firebaseapp.com",
+  projectId: "react-final-project-27904",
+  storageBucket: "react-final-project-27904.appspot.com",
+  messagingSenderId: "913470872616",
+  appId: "1:913470872616:web:5d812939e069002204291d",
+  databaseURL: "https://react-final-project-27904-default-rtdb.firebaseio.com",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
